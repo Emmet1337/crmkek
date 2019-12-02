bd007066af4eae1243621f0166a4df631bbaeca1
Emmet1337 <sardazadr@gmail.com>
2019-12-02 15:41:34 +0600

    kek

--- crmkek/settings.py
+++ crmkek/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8nsc$=ok87+e1afe_l=&1fs*0_b-o#f@ppy9s9-m7$=sbad7rh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 

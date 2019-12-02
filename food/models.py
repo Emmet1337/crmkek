c4237326776856337ab149e48b3487dbb20401cd
Emmet1337 <sardazadr@gmail.com>
2019-12-02 11:39:35 +0600

    aw yea

--- food/models.py
+++ food/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Table(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    login = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    roleid = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='users')
+    dateofadd = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    departmentid = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='categories')
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Service(models.Model):
+    percentage = models.CharField(max_length=100)
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    categoryid = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='meals')
+    price = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)

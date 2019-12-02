fc70d226b3fddeb6e395229e9ff785bbbf090acd
Emmet1337 <sardazadr@gmail.com>
2019-12-02 15:46:12 +0600

    kek

--- food/urls.py
+++ food/urls.py
@@ -3,11 +3,11 @@ from food import views
 
 
 urlpatterns = [
-    path('table', views.TableListView),
-    path('role', views.RoleListView),
-    path('department', views.DepartmentListView),
-    path('category', views.CategoryListView),
-    path('status', views.StatusListView),
-    path('service', views.ServiceListView),
-    path('meal', views.MealListView),
+    path('table', views.TableListView.as_view()),
+    path('role', views.RoleListView.as_view()),
+    path('department', views.DepartmentListView.as_view()),
+    path('category', views.CategoryListView.as_view()),
+    path('status', views.StatusListView.as_view()),
+    path('service', views.ServiceListView.as_view()),
+    path('meal', views.MealListView.as_view()),
 ]

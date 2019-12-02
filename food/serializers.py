114de5b30b5fcad20edbfe0bf970f6312bbd53c1
Emmet1337 <sardazadr@gmail.com>
2019-12-02 13:10:40 +0600

    kek

---
+++ food/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from food.models import Table, Role, Department, User, Category, Status, Service, Meal
+
+
+class TableSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Table
+        fields = ('name',)
+
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('name',)
+
+
+class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Department
+        fields = ('name',)
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'surname', 'login', 'password', 'email', 'roleid', 'dateofadd', 'phone')
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'departmentid')
+
+
+class StatusSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Status
+        fields = ('name',)
+
+
+class ServiceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Service
+        fields = ('name',)
+
+
+class MealSerializer(serializers.HyperlinkedModelSerializer):
+    # categoryid = serializers.StringRelatedField(many=False)
+
+    class Meta:
+        model = Meal
+        fields = ('name', 'categoryid', 'price', 'description')
+
+

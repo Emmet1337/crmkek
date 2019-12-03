072754451b05c937fa6d519aa5accb51f587ccab
Emmet1337 <sardazadr@gmail.com>
2019-12-03 20:56:24 +0600

    kek

--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from food import views
 
-
 urlpatterns = [
-    path('table', views.TableListView.as_view()),
+    path('table/', views.TableListView.as_view()),
     path('role', views.RoleListView.as_view()),
     path('department', views.DepartmentListView.as_view()),
     path('category', views.CategoryListView.as_view()),
--- food/views.py
+++ food/views.py
@@ -2,43 +2,67 @@ from food.serializers import *
 from rest_framework import generics, filters
 
 
-class TableListView(generics.ListAPIView):
+class TableListView(generics.ListCreateAPIView):
     serializer_class = TableSerializer
     queryset = Table.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class RoleListView(generics.ListAPIView):
+
+class RoleListView(generics.ListCreateAPIView):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class DepartmentListView(generics.ListAPIView):
+
+class DepartmentListView(generics.ListCreateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class UserListView(generics.ListAPIView):
+
+class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class CategoryListView(generics.ListAPIView):
+
+class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['departmentid__name', ]
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class StatusListView(generics.ListAPIView):
+
+class StatusListView(generics.ListCreateAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class ServiceListView(generics.ListAPIView):
+
+class ServiceListView(generics.ListCreateAPIView):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
+    def perform_create(self, serializer):
+        serializer.save()
 
-class MealListView(generics.ListAPIView):
+
+class MealListView(generics.ListCreateAPIView):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
